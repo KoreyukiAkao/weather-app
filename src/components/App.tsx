29986d4be3a6f2e2b3745b4f47b0c82a7eaa4049
KoreyukiAkao <koreyuki1007@gmail.com>
2025-02-12 00:14:45 +0900

    Organize directory structure:
    ディレクトリ構造の最適化。
    public/imagesに画像を移動
    src/assets/stylesにスタイルシート
    src/componentsにコンポーネント
    src/typesにグローバル型宣言
    その他パスの修正

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import reactLogo from "/images/react.svg";
+import viteLogo from "/images/vite.svg";
+import "../assets/styles/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
