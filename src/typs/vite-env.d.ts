a762f03c05127ad49c744e2546ebc7448053fc26
KoreyukiAkao <koreyuki1007@gmail.com>
2025-02-14 05:24:44 +0900

    Fix error about JSX elements not being recognized in TypeScript:
    JTypeScriptでJSX要素が認識されないエラーを修正

--- src/typs/vite-env.d.ts
+++ src/typs/vite-env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_WEATHER_API_KEY: string;
-  // 他の環境変数があればここに追加
+  readonly VITE_WEATHER_API_KEY: string; // 必要な環境変数を定義
 }
 
 interface ImportMeta {
